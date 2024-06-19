95161ee70718503985556745cbded3608b4f82ad
Ginormous712 <romavitaliy470@gmail.com>
2024-06-20 01:52:23 +0300

    Made config.properties, changes in utils
    Signed-off-by: Ginormous712 <romavitaliy470@gmail.com>

--- src/main/java/org/ginormous712/airline/util/DatabaseUtil.java
+++ src/main/java/org/ginormous712/airline/util/DatabaseUtil.java
@@ -1,15 +1,42 @@
 package org.ginormous712.airline.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseUtil {
-    private static final String URL = "jdbc:postgresql://localhost:5432/airline_db";
-    private static final String USER = "airline";
-    private static final String PASSWORD = "Password123";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        FileInputStream fis;
+        Properties properties = new Properties();
+
+        try {
+            fis = new FileInputStream("src/main/java/org/ginormous712/airline/util/config.properties");
+            properties.load(fis);
+            URL = properties.getProperty("db.host");
+            USER = properties.getProperty("db.user");
+            PASSWORD = properties.getProperty("db.password");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
     }
 }
